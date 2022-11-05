04ee92f7e8da66976d5e2a33d7948768f8661204
Ariel <ariel.gast@gmail.com>
2022-11-05 12:41:03 -0300

    include ItemListContainer with greeting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
+import ItemListContaniner from './components/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <ItemListContaniner name = 'Ariel' />
     </div>
   );
 }
